913839fa0c40f662bc280b51c38ca6063a1337b8
satra <satrajit.ghosh@gmail.com>
2024-01-11 16:57:12 +0000

    Deploying to gh-pages from @ ReproNim/nimh-minimal@fe058f1f3e53b5e1dbf6eddc1da55e529b549a7b 🚀

--- ui-changes/src/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/repronim/nimh-minimal/main/nimh_minimal/nimh_minimal_schema',
-  banner: 'minimal list of data collection instruments that would be ideal for use by all mental health researchers conducting clinical research to facilitate and harmonize mental health data collection.',
-  startButton: 'Join',
-  assetsPublicPath: '/nimh-minimal/',
-  backendServer: null
-};
